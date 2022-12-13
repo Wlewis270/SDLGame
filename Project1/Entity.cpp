430b61b4e073bcedb3ec61ba8931170d5a86344e
Reece Wright <rw5crt@bolton.ac.uk>
2022-12-13 13:52:19 +0000

    changes to entity

--- Project1/Entity.cpp
+++ Project1/Entity.cpp
@@ -2,3 +2,37 @@
 #include "Visualisation.h"
 #include "SDL.h"
 
+Entity::Entity(std::string Name, int maxhp, int attackvalue)
+{
+}
+
+void Entity::PerformAttack(Entity* e)
+{
+}
+
+std::string Entity::GetName()
+{
+	return std::string();
+}
+
+void Entity::TakeDamge()
+{
+}
+
+void Entity::IsDead()
+{
+}
+
+int Entity::GetHealth()
+{
+	return 0;
+}
+
+int Entity::GetMaxHealth()
+{
+	return 0;
+}
+
+void Entity::AdjustHealth(int h)
+{
+}
--- Project1/Entity.h
+++ Project1/Entity.h
@@ -1,7 +1,29 @@
 #pragma once
 #include <string>
 #include "Object.h"
+#include"HealthInterface.h"
 
-class Entity : public Object
+class Entity : public Object, public HealthInterface
 {
+	Entity(std::string Name, int maxhp,  int attackvalue);
+	void PerformAttack(Entity* e);
+	std::string GetName();
+	void TakeDamge();
+	void IsDead();
+	// Inherited via HealthInterface
+	virtual int GetHealth() override;
+
+	virtual int GetMaxHealth() override;
+
+
+	virtual void AdjustHealth(int h) override;
+
+
+	
+protected:
+	std::string m_name;
+	int m_damage;
+	int m_maxhealth;
+	int m_health;
+	
 };
